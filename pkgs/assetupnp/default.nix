a958b6833ce9b28a8b275546b7c8df483afe43ec
Tobias Pflug <tobias.pflug@gmail.com>
2022-01-02 13:27:50 +0100

    assetupnp: use local licensed copy

--- pkgs/assetupnp/default.nix
+++ pkgs/assetupnp/default.nix
@@ -2,12 +2,8 @@
 
 stdenv.mkDerivation {
   pname = "assetupnp";
-  version = "7.2";
-
-  src = fetchurl {
-    url = "https://www.dbpoweramp.com/install/AssetUPnP-Linux-x64.tar.gz";
-    sha256 = "1khshiryra1l29xgdb2xcb0p4r63b0s2nr2dnp97l80fsv0w2aj0";
-  };
+  version = "7.3";
+  src = ./AssetUPnP-Linux-x64-premium.tar.gz;
 
   buildPhase = ":";
   installPhase = ''
